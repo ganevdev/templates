a2137e1f2e43f006fd05536edecf0f25c08eef2a
Ivan Ganev <iganevru@gmail.com>
2020-01-08 23:57:01 +0300

    components folder

--- packages/cra-template-ganevru-ts/template/src/index.tsx
+++ packages/cra-template-ganevru-ts/template/src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
