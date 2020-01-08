cbd0af25705afa0e14704489b197d0221d5e697d
Ivan Ganev <iganevru@gmail.com>
2020-01-08 22:52:27 +0300

    eslint

--- packages/cra-template-ganevru-ts/template/.eslintrc.js
+++ packages/cra-template-ganevru-ts/template/.eslintrc.js
@@ -1,9 +1,10 @@
-{
-  "extends": "react-app",
-  "plugins": ["simple-import-sort", "prettier"],
-  "rules": {
+module.exports = {
+  extends: "react-app",
+  plugins: ["simple-import-sort", "prettier"],
+  rules: {
     "prettier/prettier": "warn",
     "simple-import-sort/sort": "warn",
     "sort-imports": "off"
   }
 }
+
