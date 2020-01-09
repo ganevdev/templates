036525527ae74471721a0dbcfbaeab6e40f0b49f
Ivan Ganev <iganevru@gmail.com>
2020-01-09 06:52:09 +0300

    new eslint

--- packages/cra-template-ganevru/template/.eslintrc.js
+++ packages/cra-template-ganevru/template/.eslintrc.js
@@ -1,44 +1,13 @@
 'use strict';
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-    jest: true,
-    node: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
-  ],
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaFeatures: {
-      impliedStrict: true,
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
+  extends: "react-app",
   plugins: [
-    'import',
-    'jsx-a11y',
-    'react',
-    'react-hooks',
     'simple-import-sort',
     'prettier',
   ],
-  root: true,
   rules: {
     'prettier/prettier': 'warn',
     'simple-import-sort/sort': 'warn',
-    'sort-imports': 'off',
-    'no-console': 'off',
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
+    'sort-imports': 'off'
+  }
 };
