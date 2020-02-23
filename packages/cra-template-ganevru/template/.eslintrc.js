4420513198024302a7c841e6b21722968b861abf
Ivan Ganev <iganevru@gmail.com>
2020-02-23 21:54:20 +0300

    .eslintrc.js

---
+++ packages/cra-template-ganevru/template/.eslintrc.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = {
+  extends: ['react-app'],
+  plugins: ['simple-import-sort', 'prettier'],
+  rules: {
+    'prettier/prettier': 'warn',
+    'simple-import-sort/sort': 'warn',
+    'sort-imports': 'off',
+  },
+};
